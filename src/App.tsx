c19cc9d73e53e7d60a789b369affaf48f674936a
Fernando Díaz A. <feeldiac@gmail.com>
2025-09-11 15:26:36 -0500

    fix(app): translate terms to spanish, replace words for icons, add trailing block and implement deserialization

--- src/App.tsx
+++ src/App.tsx
@@ -21,16 +21,27 @@ import {
   ItalicPlugin,
   UnderlinePlugin,
 } from "@platejs/basic-nodes/react";
-import { CaptionPlugin } from '@platejs/caption/react';
+import { CaptionPlugin } from "@platejs/caption/react";
 import { EquationPlugin, InlineEquationPlugin } from "@platejs/math/react";
+import { ImagePlugin } from "@platejs/media/react";
 import {
-  ImagePlugin,
-} from "@platejs/media/react";
-import { KEYS } from 'platejs';
+  BoldIcon,
+  Heading1Icon,
+  Heading2Icon,
+  Heading3Icon,
+  ItalicIcon,
+  QuoteIcon,
+  TrashIcon,
+  UnderlineIcon,
+} from "lucide-react";
+import {
+  deserializeHtml,
+  KEYS,
+  serializeHtml,
+  TrailingBlockPlugin,
+} from "platejs";
 import { Plate, usePlateEditor } from "platejs/react";
 import { InlineEquationToolbarButton } from "./components/ui/equation-toolbar-button";
-// import { serializeWithKatex } from "./serializeWithKatex";
-import { serializeHtml } from "platejs";
 
 const initialValue: Value = [
   {
@@ -66,12 +77,24 @@ export default function App() {
       InlineEquationPlugin.withComponent(InlineEquationElement),
       ImagePlugin.withComponent(ImageElement),
       CaptionPlugin.configure({
-      options: {
-        query: {
-          allow: [KEYS.img, KEYS.video, KEYS.audio, KEYS.file, KEYS.mediaEmbed],
+        options: {
+          query: {
+            allow: [
+              KEYS.img,
+              KEYS.video,
+              KEYS.audio,
+              KEYS.file,
+              KEYS.mediaEmbed,
+            ],
+          },
         },
-      },
-    }),
+      }),
+      TrailingBlockPlugin.configure({
+        options: {
+          type: "p", // Paragraph block
+          exclude: ["blockquote"], // Don't add after these types
+        },
+      }),
     ],
     value: () => {
       const savedValue = localStorage.getItem("installation-react-demo");
@@ -81,48 +104,70 @@ export default function App() {
 
   return (
     <>
-    <Plate
-      editor={editor}
-      onChange={({ value }) => {
-        localStorage.setItem("installation-react-demo", JSON.stringify(value));
-      }}
-    >
-      <FixedToolbar className="flex justify-start gap-1 rounded-t-lg">
-        <ToolbarButton onClick={() => editor.tf.h1.toggle()}>H1</ToolbarButton>
-        <ToolbarButton onClick={() => editor.tf.h2.toggle()}>H2</ToolbarButton>
-        <ToolbarButton onClick={() => editor.tf.h3.toggle()}>H3</ToolbarButton>
-        <ToolbarButton onClick={() => editor.tf.blockquote.toggle()}>
-          Quote
-        </ToolbarButton>
-        <InlineEquationToolbarButton>IEq</InlineEquationToolbarButton>
-        <MarkToolbarButton nodeType="bold" tooltip="Bold (⌘+B)">
-          B
-        </MarkToolbarButton>
-        <MarkToolbarButton nodeType="italic" tooltip="Italic (⌘+I)">
-          I
-        </MarkToolbarButton>
-        <MarkToolbarButton nodeType="underline" tooltip="Underline (⌘+U)">
-          U
-        </MarkToolbarButton>
-        <div className="flex-1" />
-        <ToolbarButton
-          className="px-2"
-          onClick={() => editor.tf.setValue(initialValue)}
-        >
-          Reset
-        </ToolbarButton>
-      </FixedToolbar>
-      <EditorContainer>
-        <Editor placeholder="Type your amazing content here..." />
-      </EditorContainer>
-    </Plate>
-    <button onClick={async () => {
-      const html = await serializeHtml(editor);
+      <Plate
+        editor={editor}
+        onChange={({ value }) => {
+          localStorage.setItem(
+            "installation-react-demo",
+            JSON.stringify(value)
+          );
+        }}
+      >
+        <FixedToolbar className="flex justify-start gap-1 rounded-t-lg">
+          <ToolbarButton onClick={() => editor.tf.h1.toggle()}>
+            <Heading1Icon />
+          </ToolbarButton>
+          <ToolbarButton onClick={() => editor.tf.h2.toggle()}>
+            <Heading2Icon />
+          </ToolbarButton>
+          <ToolbarButton onClick={() => editor.tf.h3.toggle()}>
+            <Heading3Icon />
+          </ToolbarButton>
+          <ToolbarButton onClick={() => editor.tf.blockquote.toggle()}>
+            <QuoteIcon />
+          </ToolbarButton>
+          <InlineEquationToolbarButton tooltip="Insertar ecuación">
+            IEq
+          </InlineEquationToolbarButton>
+          <MarkToolbarButton nodeType={KEYS.bold} tooltip="Negrita (⌘+B)">
+            <BoldIcon />
+          </MarkToolbarButton>
+          <MarkToolbarButton nodeType={KEYS.italic} tooltip="Cursiva (⌘+I)">
+            <ItalicIcon />
+          </MarkToolbarButton>
+          <MarkToolbarButton nodeType={KEYS.underline} tooltip="Subrayar (⌘+U)">
+            <UnderlineIcon />
+          </MarkToolbarButton>
+          <div className="flex-1" />
+          <ToolbarButton
+            className="px-2"
+            onClick={() => editor.tf.setValue(initialValue)}
+          >
+            {/* Limpiar */}
+            <TrashIcon />
+          </ToolbarButton>
+        </FixedToolbar>
+        <EditorContainer>
+          <Editor placeholder="Type your amazing content here..." />
+        </EditorContainer>
+      </Plate>
+      <button
+        onClick={async () => {
+          const html = await serializeHtml(editor);
 
-      console.log('html', html)
-    }} style={{ border: "1px solid red"}}>
-      Serialize
-    </button>
+          const slateValue = deserializeHtml(editor, {
+            element: html,
+          });
+
+          console.log({
+            html,
+            slateValue,
+          });
+        }}
+        style={{ border: "1px solid red" }}
+      >
+        Serialize
+      </button>
     </>
   );
 }
